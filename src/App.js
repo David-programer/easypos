f05cbc43cd1b65b91bd8efa8a2e6236bfd7a3d08
David <cirisdavi8@gmail.com>
2022-07-10 21:33:45 -0500

    Añadiendo dependencias

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <CIcon icon={icon.cilContrast} className="App-logo"  />
+        {/* <CIcon icon={icon.cilContrast} className="App-logo"  /> */}
         <p className='text-red-600'>
           EasyPos
         </p>
