c1e47f08619f2ae6ea6d3ca96dc89f74d9a99679
David <cirisdavi8@gmail.com>
2022-07-10 21:31:50 -0500

    Añadiendo dependencias

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import CIcon from '@coreui/icons-react';
+import * as icon from '@coreui/icons';
+// import {cilScreenDesktop} from '@coreui/icons';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <CIcon icon={icon.cilContrast} className="App-logo"  />
+        <p className='text-red-600'>
+          EasyPos
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p className="App-link"> Facturación e Inventario</p>
       </header>
     </div>
   );
